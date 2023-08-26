e90f6bb4b214b032725c40a3d26b3add8052a729
grzegorzsz24 <grzesio.szymanek@gmail.com>
2023-08-26 21:57:37 +0200

    created tables for movie and genre, also added test data

---
+++ src/main/java/com/example/movieclub/domain/Movie.java
@@ -0,0 +1,21 @@
+package com.example.movieclub.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String originalTitle;
+    private Integer releaseYear;
+    @ManyToOne
+    @JoinColumn(name = "genre_id", referencedColumnName = "id")
+    private Genre genre;
+    private boolean promoted;
+}
